fa95d28985298b08216f8f2295ddf36f0246be72
jesse <jsoldat@hotmail.com>
2017-06-03 18:32:16 -0400

    added reveals for four characters

--- main.js
+++ main.js
@@ -1,3 +1,48 @@
 window.sr = ScrollReveal();
-sr.reveal('.hinata');
-sr.reveal('.neji');
+
+sr.reveal('.navbar', {
+	duration: 2000,
+	origin: 'bottom'
+});
+sr.reveal('.hinata', {
+	duration: 1500,
+	origin: 'left',
+	delay: 1000,
+	distance: '300px'
+});
+sr.reveal('.neji', {
+	duration: 1500,
+	origin: 'right',
+	delay: 2000,
+	distance: '300px'
+});
+
+sr.reveal('.hyuga', {
+	duration: 3000,
+	origin: 'bottom',
+	delay: 3000,
+	distance: '300px',
+	viewFactor: 0.2
+});
+
+sr.reveal('.sasuke', {
+	duration: 2000,
+	origin: 'top',
+	distance: '300px'
+});
+
+
+sr.reveal('.itachi', {
+	duration: 2000,
+	delay: 1000,
+	origin: 'right',
+	distance: '300px'
+});
+
+sr.reveal('.mangekyou', {
+	duration: 3000,
+	origin: 'bottom',
+	delay: 2000,
+	distance: '300px',
+	viewFactor: 0.2
+});
