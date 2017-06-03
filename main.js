2dfa43988bc02e0bed2cb47f5b07368dfce6baf7
jesse <jsoldat@hotmail.com>
2017-06-03 19:41:08 -0400

    added a banner

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ sr.reveal('.hyuga', {
 	origin: 'bottom',
 	delay: 3000,
 	distance: '300px',
-	viewFactor: 0.2
+	// viewFactor: 0.2
 });
 
 sr.reveal('.sasuke', {
@@ -44,5 +44,32 @@ sr.reveal('.mangekyou', {
 	origin: 'bottom',
 	delay: 2000,
 	distance: '300px',
-	viewFactor: 0.2
+	// viewFactor: 0.2
+});
+
+
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 });
